95c96f4a09bf8a015f9f56e539250e5e261abc71
daimaru-hakui <dh.mukai.dh@gmail.com>
2023-01-19 11:16:01 +0900

    機能項目を追加

--- src/pages/products/edit/[productId].tsx
+++ src/pages/products/edit/[productId].tsx
@@ -1,41 +1,7 @@
-import {
-  Box,
-  Button,
-  Checkbox,
-  CheckboxGroup,
-  Container,
-  Divider,
-  Flex,
-  FormControl,
-  FormLabel,
-  Input,
-  NumberDecrementStepper,
-  NumberIncrementStepper,
-  NumberInput,
-  NumberInputField,
-  NumberInputStepper,
-  Radio,
-  RadioGroup,
-  Select,
-  Stack,
-  Text,
-  Textarea,
-} from "@chakra-ui/react";
-import {
-  addDoc,
-  collection,
-  doc,
-  getDoc,
-  getDocs,
-  orderBy,
-  query,
-} from "firebase/firestore";
+import { doc, getDoc } from "firebase/firestore";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
-import { useRecoilValue, useSetRecoilState } from "recoil";
-import { colors, features, materialNames } from "../../../../datalist";
 import { db } from "../../../../firebase";
-import MaterialsModal from "../../../components/products/MaterialsModal";
 import ProductInputArea from "../../../components/products/ProductInputArea";
 
 const ProductsEdit = () => {
