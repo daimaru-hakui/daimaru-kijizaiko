73b388e02f6cacc650be0e0c59e9ffe3f35cdd53
sukebon <39957486+sukebon@users.noreply.github.com>
2023-02-22 00:28:30 +0900

    dashboard

--- src/components/toppage/StatCard.tsx
+++ src/components/toppage/StatCard.tsx
@@ -1,23 +1,31 @@
-import { Box, Flex, Stat, StatLabel, StatNumber } from '@chakra-ui/react'
+import { Box, Flex, Stat, StatHelpText, StatLabel, StatNumber } from '@chakra-ui/react'
 import { NextPage } from 'next';
 type Props = {
   title: string;
   quantity: string | number;
   unit: string;
+  price: string | number
+  subUnit: string
+  fontSize: string
 }
 
-const StatCard: NextPage<Props> = ({ title, quantity, unit }) => {
+const StatCard: NextPage<Props> = ({ title, quantity, unit, price, subUnit, fontSize }) => {
 
   return (
     <>
       <Stat p={6} bg="white" rounded="md" boxShadow="md">
         <StatLabel>{title}</StatLabel>
-        <StatNumber fontSize="4xl">
+        <StatNumber fontSize={fontSize}>
           {quantity}
           <Box as="span" fontSize="sm" ml={1}>
             {unit}
           </Box>
         </StatNumber>
+        {price && (
+          <StatHelpText fontSize="lg">{price}
+            <Box as="span" ml={1}>{subUnit}</Box>
+          </StatHelpText>
+        )}
       </Stat>
     </>
   )
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -7,12 +7,13 @@ import {
   StatNumber,
 } from "@chakra-ui/react";
 import { useRecoilValue } from 'recoil'
-import { fabricDyeingOrdersState, fabricPurchaseOrdersState, grayFabricOrdersState, productsState } from '../../../store'
+import { currentUserState, fabricDyeingOrdersState, fabricPurchaseOrdersState, grayFabricOrdersState, productsState } from '../../../store'
 import { ProductType } from '../../../types/FabricType'
 import StatCard from "../../components/toppage/StatCard";
 
 
 const Dashboard = () => {
+  const currentUser = useRecoilValue(currentUserState)
   const products = useRecoilValue(productsState)
   const grayFabricOrders = useRecoilValue(grayFabricOrdersState)
   const fabricDyeingOrders = useRecoilValue(fabricDyeingOrdersState)
@@ -38,110 +39,122 @@ const Dashboard = () => {
   };
 
   return (
-    <Box w="100%" mt={12}>
-      <Container maxW="1200px" my={6}>
-        <Flex mt={6} gap={6} flexDirection={{ base: "column", sm: "row" }}>
-          <StatCard
-            title="キバタ仕掛数"
-            quantity={grayFabricOrders?.length}
-            unit="件"
-          />
-          <StatCard
-            title="生地仕掛数"
-            quantity={fabricDyeingOrders?.length}
-            unit="件"
-          />
-          <StatCard
-            title="入荷予定数"
-            quantity={fabricPurchaseOrders?.length}
-            unit="件"
-          />
-        </Flex>
+    <>
+      {currentUser && (
+        <Box w="100%" mt={12}>
+          <Container maxW="1200px" my={6}>
+            <Flex mt={6} gap={6} flexDirection={{ base: "column", sm: "row" }}>
+              <StatCard
+                title="キバタ仕掛数"
+                quantity={grayFabricOrders?.length}
+                unit="件"
+                price=""
+                subUnit=""
+                fontSize="3xl"
+              />
+              <StatCard
+                title="生地仕掛数"
+                quantity={fabricDyeingOrders?.length}
+                unit="件"
+                price=""
+                subUnit=""
+                fontSize="3xl"
+              />
+              <StatCard
+                title="入荷予定数"
+                quantity={fabricPurchaseOrders?.length}
+                unit="件"
+                price=""
+                subUnit=""
+                fontSize="3xl"
+              />
+            </Flex>
 
-        <Flex mt={6} gap={6} flexDirection={{ base: "column", md: "row" }}>
-          <StatCard
-            title="TOTAL数量"
-            quantity={
-              getTotalProductsQuantity(
-                ["wip", "externalStock", "arrivingQuantity", "tokushimaStock"]
-              ).toFixed(2)
-            }
-            unit="m"
-          />
-          <StatCard
-            title="TOTAL在庫"
-            quantity={
-              Number(getTotalProductsPrice(
-                ["wip", "externalStock", "arrivingQuantity", "tokushimaStock"]
-              ).toFixed()).toLocaleString()
-            }
-            unit="円"
-          />
+            <Flex mt={6} gap={6} flexDirection={{ base: "column", md: "row" }}>
+              <StatCard
+                title="TOTAL数量"
+                quantity={
+                  getTotalProductsQuantity(
+                    ["wip", "externalStock", "arrivingQuantity", "tokushimaStock"]
+                  ).toFixed(2)
+                }
+                unit="m"
+                price=""
+                subUnit=""
+                fontSize="4xl"
+              />
+              <StatCard
+                title="TOTAL在庫"
+                quantity={
+                  Number(getTotalProductsPrice(
+                    ["wip", "externalStock", "arrivingQuantity", "tokushimaStock"]
+                  ).toFixed()).toLocaleString()
+                }
+                unit="円"
+                price=""
+                subUnit=""
+                fontSize="4xl"
+              />
 
-        </Flex>
+            </Flex>
 
-        <Flex gap={{ base: 0, md: 6 }} flexDirection={{ base: "column", md: "row" }}>
-          <Flex flex="1" mt={6} gap={6} flexDirection={{ base: "row", md: "column" }}>
-            <StatCard
-              title="仕掛数量"
-              quantity={getTotalProductsQuantity(["wip"]).toFixed()}
-              unit="m"
-            />
-            <StatCard
-              title="仕掛金額"
-              quantity={Number(getTotalProductsPrice(["wip"]).toFixed())}
-              unit="円"
-            />
-          </Flex>
-          <Flex flex="1" mt={6} gap={6} flexDirection={{ base: "row", md: "column" }}>
-            <StatCard
-              title="外部在庫数量"
-              quantity={getTotalProductsQuantity(["externalStock"]).toFixed()}
-              unit="m"
-            />
-            <StatCard
-              title="外部在庫金額"
-              quantity={Number(
-                getTotalProductsPrice(["externalStock"]).toFixed()
-              ).toLocaleString()}
-              unit="円"
-            />
-          </Flex>
+            <Flex gap={{ base: 0, md: 6 }} flexDirection={{ base: "column", md: "row" }}>
+              <Flex flex="1" mt={6} gap={6} flexDirection={{ base: "row" }}>
+                <StatCard
+                  title="仕掛"
+                  quantity={getTotalProductsQuantity(["wip"]).toFixed()}
+                  unit="m"
+                  price={Number(getTotalProductsPrice(["wip"]).toFixed()).toLocaleString()}
+                  subUnit="円"
+                  fontSize="3xl"
+                />
 
-          <Flex flex="1" mt={6} gap={6} flexDirection={{ base: "row", md: "column" }}>
-            <StatCard
-              title="入荷待ち数量"
-              quantity={getTotalProductsQuantity(["arrivingQuantity"]).toFixed()}
-              unit="m"
-            />
-            <StatCard
-              title="入荷待ち金額"
-              quantity={
-                (getTotalProductsPrice(["arrivingQuantity"]).toFixed()
-                ).toLocaleString()}
-              unit="円"
-            />
-          </Flex>
 
-          <Flex flex="1" mt={6} gap={6} flexDirection={{ base: "row", md: "column" }}>
-            <StatCard
-              title="在庫数量"
-              quantity={getTotalProductsQuantity(["tokushimaStock"]).toFixed(2)}
-              unit="m"
-            />
+                <StatCard
+                  title="外部在庫"
+                  quantity={getTotalProductsQuantity(["externalStock"]).toFixed()}
+                  unit="m"
+                  price={Number(
+                    getTotalProductsPrice(["externalStock"]).toFixed()
+                  ).toLocaleString()}
+                  subUnit="円"
+                  fontSize="3xl"
+                />
 
-            <StatCard
-              title="在庫金額"
-              quantity={Number(
-                getTotalProductsPrice(["tokushimaStock"]).toFixed()
-              ).toLocaleString()}
-              unit="円"
-            />
+              </Flex>
 
-          </Flex>
-        </Flex>
-      </Container>
-    </Box>
+              <Flex flex="1" mt={6} gap={6} flexDirection={{ base: "row" }}>
+                <StatCard
+                  title="入荷待ち"
+                  quantity={getTotalProductsQuantity(["arrivingQuantity"]).toFixed()}
+                  unit="m"
+                  price={
+                    (getTotalProductsPrice(["arrivingQuantity"]).toFixed()
+                    ).toLocaleString()}
+                  subUnit="円"
+                  fontSize="3xl"
+                />
+
+
+
+
+                <StatCard
+                  title="徳島在庫"
+                  quantity={getTotalProductsQuantity(["tokushimaStock"]).toFixed(2)}
+                  unit="m"
+                  price={Number(
+                    getTotalProductsPrice(["tokushimaStock"]).toFixed()
+                  ).toLocaleString()}
+                  subUnit="円"
+                  fontSize="3xl"
+                />
+
+              </Flex>
+            </Flex>
+          </Container>
+        </Box>
+      )}
+    </>
   )
 }
 
