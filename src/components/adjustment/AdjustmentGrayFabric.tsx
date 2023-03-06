8d3c46de6b66d1e1bb0377aa66e7f028f1060d9b
daimaru-hakui <dh.mukai.dh@gmail.com>
2023-03-06 14:47:44 +0900

    API

---
+++ src/components/adjustment/AdjustmentGrayFabric.tsx
@@ -0,0 +1,116 @@
+import {
+  Box,
+  Button,
+  Flex,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  Td,
+  Tr,
+} from "@chakra-ui/react";
+import { GiCancel } from "react-icons/gi";
+import { NextPage } from "next";
+import { useEffect } from "react";
+import { useGetDisp } from "../../hooks/UseGetDisp";
+import { useInputProduct } from "../../hooks/UseInputProduct";
+import { useUtil } from "../../hooks/UseUtil";
+import { GrayFabricType } from "../../../types/GrayFabricType";
+import { useGrayFabricFunc } from "../../hooks/UseGrayFabricFunc";
+import { useInputGrayFabric } from "../../hooks/UseInputGrayFabric";
+
+type Props = {
+  grayFabric: GrayFabricType;
+};
+
+const AdjustmentGrayFabric: NextPage<Props> = ({ grayFabric }) => {
+  const { getUserName } = useGetDisp(); //// ？
+  const { items, setItems, handleNumberChange } = useInputGrayFabric();
+  const { updateAjustmentGrayFabric, onReset } = useGrayFabricFunc(
+    items,
+    setItems
+  );
+  const { quantityValueBold } = useUtil();
+
+  useEffect(() => {
+    setItems({ ...grayFabric } as any);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [grayFabric.price, grayFabric.stock]);
+
+  return (
+    <Tr key={grayFabric.id}>
+      <Td>{grayFabric.productNumber}</Td>
+
+      <Td p={1} isNumeric>
+        <NumberInput
+          mt={1}
+          w="90px"
+          name="price"
+          defaultValue={0}
+          min={0}
+          max={100000}
+          value={items.price}
+          onChange={(e) => handleNumberChange(e, "price")}
+        >
+          <NumberInputField textAlign="right" />
+          <NumberInputStepper>
+            <NumberIncrementStepper />
+            <NumberDecrementStepper />
+          </NumberInputStepper>
+        </NumberInput>
+      </Td>
+      <Td p={1} isNumeric fontWeight={quantityValueBold(grayFabric?.wip)}>
+        <NumberInput
+          mt={1}
+          w="90px"
+          name="wip"
+          defaultValue={0}
+          min={0}
+          max={100000}
+          value={items.wip}
+          onChange={(e) => handleNumberChange(e, "wip")}
+        >
+          <NumberInputField textAlign="right" />
+          <NumberInputStepper>
+            <NumberIncrementStepper />
+            <NumberDecrementStepper />
+          </NumberInputStepper>
+        </NumberInput>
+      </Td>
+      <Td p={1} isNumeric fontWeight={quantityValueBold(grayFabric?.stock)}>
+        <NumberInput
+          mt={1}
+          w="90px"
+          name="stock"
+          defaultValue={0}
+          min={0}
+          max={100000}
+          value={items.stock}
+          onChange={(e) => handleNumberChange(e, "stock")}
+        >
+          <NumberInputField textAlign="right" />
+          <NumberInputStepper>
+            <NumberIncrementStepper />
+            <NumberDecrementStepper />
+          </NumberInputStepper>
+        </NumberInput>
+      </Td>
+
+      <Td flex="1">
+        <Flex alignItems="center" gap={3}>
+          <Button
+            size="xs"
+            colorScheme="facebook"
+            onClick={() => updateAjustmentGrayFabric(grayFabric.id)}
+          >
+            更新
+          </Button>
+          <GiCancel cursor="pointer" onClick={() => onReset(grayFabric)} />
+        </Flex>
+      </Td>
+    </Tr>
+  );
+};
+
+export default AdjustmentGrayFabric;
---
+++ src/pages/api/cutting-reports/index.ts
@@ -0,0 +1,33 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { db } from "../../../../firebase/sever";
+import { CuttingReportType } from "../../../../types/CuttingReportType";
+
+type Data = {
+  contents: CuttingReportType[];
+  count?: FirebaseFirestore.AggregateField<number>;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data | string>
+) {
+  if (!process.env.BACKEND_API_KEY) return res.status(405).json("error");
+  if (req.method === "GET") {
+    const querySnapshot = await db
+      .collection("cuttingReports")
+      .orderBy("cuttingDate", "desc")
+      .get();
+    const contents = querySnapshot.docs.flatMap(
+      (doc) => ({ ...doc.data(), id: doc.id } as CuttingReportType)
+    );
+    return res.status(200).json({ contents });
+
+    // const query = db.collection("cuttingReports").orderBy("cuttingDate",'desc')
+    // query.onSnapshot(querySnapshot => {
+    //  const contents = querySnapshot.docs.map((doc) => (
+    //     ({ ...doc.data(), id: doc.id } as CuttingReportType)
+    //   ))
+    //   return res.status(200).json({ contents });
+    // } )
+  }
+}
